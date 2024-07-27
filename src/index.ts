cc16649ce598be768c9e937486c5b10d956b3869
TrpFrog <dev@trpfrog.net>
2024-07-27 21:36:40 +0900

    Add rate limit

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
+import { rateLimiter } from "./middlewares/rateLimiter";
 import { factory } from "./factory";
 import v1 from "./v1";
 
-const app = factory.createApp().route("/v1", v1);
+const app = factory.createApp().use(rateLimiter).route("/v1", v1);
 
 export default app;
---
+++ src/middlewares/rateLimiter.ts
@@ -0,0 +1,9 @@
+import { cloudflareRateLimiter } from "@hono-rate-limiter/cloudflare";
+import { AppType, factory } from "../factory";
+
+export const rateLimiter = factory.createMiddleware(async (c, next) => {
+  cloudflareRateLimiter<AppType>({
+    rateLimitBinding: c.env.RATE_LIMITER,
+    keyGenerator: (c) => c.req.header("cf-connecting-ip") ?? "", // Method to generate custom identifiers for clients.
+  })(c, next);
+});
