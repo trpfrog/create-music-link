142ddc890b2dcecdabacafcb3385e0fb2bc37114
TrpFrog <dev@trpfrog.net>
2024-08-17 19:25:18 +0900

    Update to align with Lynkify's specification changes

--- src/fetch.ts
+++ src/fetch.ts
@@ -1,17 +1,51 @@
 import { Market, SpotifyApi, Track } from "@spotify/web-api-ts-sdk";
 import { marketSchema } from "./spotify/schema";
+import { z } from "zod";
+
+const lynkifyResponseSchema = z.object({
+  songType: z.string(),
+  songName: z.string(),
+  songId: z.string(),
+});
+
+function parseLynkifyResponse(rawResponseBody: string) {
+  const rawJson = rawResponseBody.split("\n")[1].slice(2);
+  const json = JSON.parse(rawJson);
+  return lynkifyResponseSchema.parse(json);
+}
 
 export async function fetchLynkifyUrl(musicProviderUrl: string) {
-  const url = new URL("https://lynkify.in/create");
-  url.searchParams.append("url", musicProviderUrl);
-  const res = await fetch(url);
-  if (res.ok) {
-    return res.url;
-  } else {
+  const res = await fetch("https://lynkify.in/create-link", {
+    body: JSON.stringify([musicProviderUrl]),
+    method: "POST",
+    headers: {
+      "next-action": "b40862cfb950af1504cc8b997391f8c65d627b2c",
+    },
+  });
+
+  if (!res.ok) {
     throw new Error("Failed to fetch lynkify url", {
       cause: res,
     });
   }
+
+  if (res.headers.get("content-type") !== "text/x-component") {
+    throw new Error("Unexpected content type from Lynkify", {
+      cause: res,
+    });
+  }
+
+  const { songType, songName, songId } = await res
+    .text()
+    .then(parseLynkifyResponse)
+    .catch((e) => {
+      throw new Error("Failed to parse Lynkify response", {
+        cause: e,
+      });
+    });
+
+  const url = `https://lynkify.in/${songType}/${songName}/${songId}`;
+  return url;
 }
 
 export async function fetchSpotifyTrackIdFromLykinfyUrl(lynkifyUrl: string) {
