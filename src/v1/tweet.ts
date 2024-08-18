5d1caf57f96bac13c36038839a7bb2adceeecf3b
TrpFrog <dev@trpfrog.net>
2024-08-18 23:53:51 +0900

    Add template option to generateNowPlayingTweet

--- src/v1/tweet.ts
+++ src/v1/tweet.ts
@@ -1,14 +1,26 @@
 import { Track } from "@spotify/web-api-ts-sdk";
+import { fillTemplate, joinWithAnd } from "../utils";
 
-export function generateNowPlaygroundTweet(track: Track, lynkifyUrl: string) {
-  const artists = track.artists.map((a) => a.name).join(" & ");
-  const albumArtists = track.album.artists.map((a) => a.name).join(" & ");
-  const tweet = [
-    `#nowplaying ${artists} - ${track.name} (${albumArtists} - ${track.album.name})`,
-    lynkifyUrl,
-  ].join("\n");
+export function generateNowPlaygroundTweet(
+  track: Track,
+  lynkifyUrl: string,
+  options?: {
+    template?: string;
+  }
+) {
+  const template =
+    options?.template ||
+    "#nowplaying ${artists} - ${trackName} (${albumArtists} - ${albumName})\n${url}";
 
-  return tweet;
+  return fillTemplate(template, {
+    artists: joinWithAnd(track.artists.map((a) => a.name)),
+    albumArtists: joinWithAnd(track.album.artists.map((a) => a.name)),
+    trackName: track.name,
+    albumName: track.album.name,
+    minutes: Math.floor(track.duration_ms / 1000 / 60),
+    seconds: Math.floor((track.duration_ms / 1000) % 60),
+    url: lynkifyUrl,
+  });
 }
 
 export function createNowPlaygroundTweetUrl(track: Track, lynkifyUrl: string) {
